01bc0954b092ca986edfda252e49dd73209ac659
adrien <adrien@bouvais.lu>
2026-05-17 21:20:35 +0200

    Update lib.zig

--- src/lib.zig
+++ src/lib.zig
@@ -1,2 +1,4 @@
 pub const GpuAllocator = @import("GpuAllocator.zig");
 pub const GpuBuffer = @import("GpuBuffer.zig");
+pub const GpuDevice = @import("GpuDevice.zig");
+pub const GpuPipeline = @import("GpuPipeline.zig");
